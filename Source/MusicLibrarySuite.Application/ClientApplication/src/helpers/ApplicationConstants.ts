f63230d4062b4b59909b0bd7d324f0d8b33d7231
Andrey Talanin <andrey.talanin@outlook.com>
2022-11-20 13:47:18 +0300

    Add the ApplicationConstants.ts file with commonly-used constants

---
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/helpers/ApplicationConstants.ts
@@ -0,0 +1,6 @@
+export const EmptyDateTimeString = "0001-01-01T00:00:00";
+export const EmptyDateTimeOffsetString = "0001-01-01T00:00:00.0000000+00:00";
+export const EmptyGuidString = "00000000-0000-0000-0000-000000000000";
+
+export const EmptyDateTime = new Date(EmptyDateTimeString);
+export const EmptyDateTimeOffset = new Date(EmptyDateTimeOffsetString);
