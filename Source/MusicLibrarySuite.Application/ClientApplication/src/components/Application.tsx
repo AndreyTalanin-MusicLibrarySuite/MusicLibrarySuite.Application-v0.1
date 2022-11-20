dd1b245a25f5c45f2a16a0e51994ed9a74ecbf2a
Andrey Talanin <andrey.talanin@outlook.com>
2022-11-20 15:56:15 +0300

    Add pages to the router configuration

--- Source/MusicLibrarySuite.Application/ClientApplication/src/components/Application.tsx
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/components/Application.tsx
@@ -4,18 +4,35 @@ import { BrowserRouter } from "react-router-dom";
 import ApplicationLayout from "./ApplicationLayout";
 import ApplicationMenuItemDescriptor from "../entities/ApplicationMenuItemDescriptor";
 import ApplicationPageDescriptor from "../entities/ApplicationPageDescriptor";
+import EditGenrePage, { EditGenrePageMode } from "../pages/catalog/genres/EditGenrePage";
+import GenreListPage from "../pages/catalog/genres/GenreListPage";
+import ViewGenrePage from "../pages/catalog/genres/ViewGenrePage";
 import HomePage from "../pages/HomePage";
 import InvalidRoutePage from "../pages/InvalidRoutePage";
 
 // prettier-ignore
 const applicationPageDescriptors: ApplicationPageDescriptor[] = [
   { key: "home-page", path: "/home", name: "Home", componentFactory: () => <HomePage /> },
+  { key: "catalog-node", path: "/catalog", name: "Catalog" },
+  { key: "catalog-genre-node", path: "/catalog/genres", name: "Genres" },
+  { key: "catalog-genre-list-page", path: "/catalog/genres/list", name: "Browse All", componentFactory: () => <GenreListPage /> },
+  { key: "catalog-genre-view-page", path: "/catalog/genres/view", name: "View", componentFactory: () => <ViewGenrePage /> },
+  { key: "catalog-genre-create-page", path: "/catalog/genres/create", name: "Create", componentFactory: () => <EditGenrePage mode={EditGenrePageMode.Create} />, },
+  { key: "catalog-genre-edit-page", path: "/catalog/genres/edit", name: "Edit", componentFactory: () => <EditGenrePage mode={EditGenrePageMode.Edit} /> },
   { key: "invalid-route-page", path: "*", name: "Invalid Route", componentFactory: () => <InvalidRoutePage /> },
 ];
 
 // prettier-ignore
 const applicationMenuItemDescriptors: ApplicationMenuItemDescriptor[] = [
   { key: "home-page", label: "Home", type: "item" },
+  {
+    key: "catalog-node",
+    label: "Catalog",
+    type: "menu",
+    items: [
+      { key: "catalog-genre-list-page", label: "Genres", type: "item" },
+    ],
+  },
 ];
 
 const Application = () => {
