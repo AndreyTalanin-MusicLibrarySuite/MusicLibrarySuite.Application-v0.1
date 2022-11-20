d038f750d53c5d1c1d03b0dbcb91448f9e433f58
Andrey Talanin <andrey.talanin@outlook.com>
2022-11-20 15:55:06 +0300

    Add the ViewGenrePage React.js page component

---
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/genres/ViewGenrePage.tsx
@@ -0,0 +1,91 @@
+import { Button, Card, Divider, Space, Tag, Typography } from "antd";
+import { EditOutlined, RollbackOutlined } from "@ant-design/icons";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { Genre } from "../../../api/ApplicationClient";
+import useApplicationClient from "../../../hooks/useApplicationClient";
+import useQueryStringId from "../../../hooks/useQueryStringId";
+import styles from "./ViewGenrePage.module.css";
+import "antd/dist/antd.min.css";
+
+const { Paragraph, Text, Title } = Typography;
+
+const ViewGenrePage = () => {
+  const navigate = useNavigate();
+
+  const [genre, setGenre] = useState<Genre | undefined>(new Genre());
+
+  const [id] = useQueryStringId(true);
+  const applicationClient = useApplicationClient();
+
+  const fetchGenre = useCallback(() => {
+    if (id !== undefined) {
+      applicationClient
+        .getGenre(id)
+        .then((genre) => {
+          setGenre(genre);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  }, [id, applicationClient]);
+
+  useEffect(() => {
+    fetchGenre();
+  }, [fetchGenre]);
+
+  const onEditButtonClick = () => {
+    navigate(`/catalog/genres/edit?id=${id}`);
+  };
+
+  const onCancelButtonClick = () => {
+    navigate("/catalog/genres/list");
+  };
+
+  return (
+    <>
+      <Space className={styles.pageHeader} direction="horizontal" align="baseline">
+        <Title level={4}>View Genre</Title>
+        <Button type="primary" onClick={onEditButtonClick}>
+          <EditOutlined />
+          Edit
+        </Button>
+        <Button onClick={onCancelButtonClick}>
+          <RollbackOutlined />
+          Back to Genre List
+        </Button>
+      </Space>
+      {genre && (
+        <Card
+          title={
+            <Space>
+              {genre.name}
+              {genre.systemEntity && <Tag>System Entity</Tag>}
+              <Tag color={genre.enabled ? "green" : "red"}>{genre.enabled ? "Enabled" : "Disabled"}</Tag>
+            </Space>
+          }
+        >
+          {genre.description?.length && (
+            <>
+              <Paragraph>{genre.description}</Paragraph>
+              <Divider />
+            </>
+          )}
+          {genre.createdOn && (
+            <Paragraph>
+              Created On: <Text keyboard>{genre.createdOn.toLocaleString()}</Text>
+            </Paragraph>
+          )}
+          {genre.updatedOn && (
+            <Paragraph>
+              Updated On: <Text keyboard>{genre.updatedOn.toLocaleString()}</Text>
+            </Paragraph>
+          )}
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default ViewGenrePage;
