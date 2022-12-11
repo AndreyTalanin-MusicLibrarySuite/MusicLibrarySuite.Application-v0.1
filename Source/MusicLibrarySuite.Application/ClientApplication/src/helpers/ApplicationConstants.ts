bc9b878b93702b549ffef39df0431e3d4560bd9b
Andrey Talanin <andrey.talanin@outlook.com>
2022-12-11 19:43:59 +0300

    Add the ArtistListPage React.js page component

--- Source/MusicLibrarySuite.Application/ClientApplication/src/helpers/ApplicationConstants.ts
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/helpers/ApplicationConstants.ts
@@ -4,3 +4,6 @@ export const EmptyGuidString = "00000000-0000-0000-0000-000000000000";
 
 export const EmptyDateTime = new Date(EmptyDateTimeString);
 export const EmptyDateTimeOffset = new Date(EmptyDateTimeOffsetString);
+
+export const DefaultPageSize = 10;
+export const DefaultPageIndex = 0;
