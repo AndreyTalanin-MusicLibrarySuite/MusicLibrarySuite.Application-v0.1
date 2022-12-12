8ea267bcefe8e85e741a36150fa81c2be1873bb9
Andrey Talanin <andrey.talanin@outlook.com>
2022-12-12 22:24:55 +0300

    Add the ArtistViewPageArtistRelationshipsTab React.js component

---
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/artists/ArtistViewPageArtistRelationshipsTab.tsx
@@ -0,0 +1,60 @@
+import { Checkbox, Typography } from "antd";
+import { useCallback, useEffect, useState } from "react";
+import { ArtistRelationship } from "../../../api/ApplicationClient";
+import EntityRelationshipTable from "../../../components/tables/EntityRelationshipTable";
+import useApplicationClient from "../../../hooks/useApplicationClient";
+import "antd/dist/antd.min.css";
+
+export interface ArtistViewPageArtistRelationshipsTabProps {
+  id: string;
+}
+
+const ArtistViewPageArtistRelationshipsTab = ({ id }: ArtistViewPageArtistRelationshipsTabProps) => {
+  const [artistRelationships, setArtistRelationships] = useState<ArtistRelationship[]>([]);
+  const [artistRelationshipsLoading, setArtistRelationshipsLoading] = useState<boolean>(true);
+  const [includeReverseRelationships, setIncludeReverseRelationships] = useState<boolean>(false);
+
+  const applicationClient = useApplicationClient();
+
+  const fetchArtistRelationships = useCallback(() => {
+    setArtistRelationshipsLoading(true);
+    applicationClient
+      .getArtistRelationships(id, includeReverseRelationships)
+      .then((artistRelationships) => {
+        setArtistRelationships(artistRelationships);
+        setArtistRelationshipsLoading(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [id, includeReverseRelationships, applicationClient]);
+
+  useEffect(() => {
+    fetchArtistRelationships();
+  }, [fetchArtistRelationships]);
+
+  return (
+    <>
+      <Typography.Paragraph>
+        <Checkbox onChange={(e) => setIncludeReverseRelationships(e.target.checked)}>Include reverse artist relationships</Checkbox>
+      </Typography.Paragraph>
+      <EntityRelationshipTable
+        entityColumnName="Artist"
+        dependentEntityColumnName="Dependent Artist"
+        loading={artistRelationshipsLoading}
+        entityRelationships={artistRelationships.map((artistRelationship) => ({
+          name: artistRelationship.name,
+          description: artistRelationship.description,
+          entityId: artistRelationship.artistId,
+          entityName: artistRelationship.artist?.name ?? "",
+          entityHref: `/catalog/artists/view?id=${artistRelationship.artistId}`,
+          dependentEntityId: artistRelationship.dependentArtistId,
+          dependentEntityName: artistRelationship.dependentArtist?.name ?? "",
+          dependentEntityHref: `/catalog/artists/view?id=${artistRelationship.dependentArtistId}`,
+        }))}
+      />
+    </>
+  );
+};
+
+export default ArtistViewPageArtistRelationshipsTab;
