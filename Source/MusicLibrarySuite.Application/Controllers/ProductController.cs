27c8bb4d0614408b2028654ad4c6af2709224f83
Andrey Talanin <andrey.talanin@outlook.com>
2022-12-24 14:28:45 +0300

    Add the ProductController Web API controller class

---
+++ Source/MusicLibrarySuite.Application/Controllers/ProductController.cs
@@ -0,0 +1,168 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using MusicLibrarySuite.CatalogService.Client;
+using MusicLibrarySuite.CatalogService.Interfaces.Entities;
+
+namespace MusicLibrarySuite.Application.Controllers;
+
+/// <summary>
+/// Represents a Web API controller for the <see cref="Product" /> entity.
+/// </summary>
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class ProductController : ControllerBase
+{
+    private readonly ICatalogServiceClient m_catalogServiceClient;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProductController" /> type using the specified services.
+    /// </summary>
+    /// <param name="catalogServiceClient">The catalog service client.</param>
+    public ProductController(ICatalogServiceClient catalogServiceClient)
+    {
+        m_catalogServiceClient = catalogServiceClient;
+    }
+
+    /// <summary>
+    /// Asynchronously gets a product by its unique identifier.
+    /// </summary>
+    /// <param name="productId">The product's unique identifier.</param>
+    /// <returns>
+    /// The task object representing the asynchronous operation.
+    /// The task's result will be the product found or <see langword="null" />.
+    /// </returns>
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Product>> GetProductAsync([Required][FromQuery] Guid productId)
+    {
+        try
+        {
+            Product product = await m_catalogServiceClient.GetProductAsync(productId);
+            return Ok(product);
+        }
+        catch (ApiException apiException) when (apiException.StatusCode == StatusCodes.Status404NotFound)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously gets products by an array of unique identifiers.
+    /// </summary>
+    /// <param name="productIds">The array of unique identifiers to search for.</param>
+    /// <returns>
+    /// The task object representing the asynchronous operation.
+    /// The task's result will be an array containing all found products.
+    /// </returns>
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<Product[]>> GetProductsAsync([Required][FromQuery] Guid[] productIds)
+    {
+        Product[] productArray = (await m_catalogServiceClient.GetProductsAsync(productIds)).ToArray();
+        return Ok(productArray);
+    }
+
+    /// <summary>
+    /// Asynchronously gets all products.
+    /// </summary>
+    /// <returns>
+    /// The task object representing the asynchronous operation.
+    /// The task's result will be an array containing all products.
+    /// </returns>
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<Product[]>> GetAllProductsAsync()
+    {
+        Product[] productArray = (await m_catalogServiceClient.GetAllProductsAsync()).ToArray();
+        return Ok(productArray);
+    }
+
+    /// <summary>
+    /// Asynchronously gets products by a product page request.
+    /// </summary>
+    /// <param name="pageSize">The page size.</param>
+    /// <param name="pageIndex">The page index.</param>
+    /// <param name="title">The filter value for the <see cref="Product.Title" /> property.</param>
+    /// <param name="enabled">The filter value for the <see cref="Product.Enabled" /> property.</param>
+    /// <returns>
+    /// The task object representing the asynchronous operation.
+    /// The task's result will be an array containing all products corresponding to the request configuration.
+    /// </returns>
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<ProductPageResponse>> GetPagedProductsAsync([Required][FromQuery] int pageSize, [Required][FromQuery] int pageIndex, [FromQuery] string? title, [FromQuery] bool? enabled)
+    {
+        ProductPageResponse pageResponse = await m_catalogServiceClient.GetPagedProductsAsync(pageSize, pageIndex, title, enabled);
+        return Ok(pageResponse);
+    }
+
+    /// <summary>
+    /// Asynchronously creates a new product.
+    /// </summary>
+    /// <param name="product">The product to create.</param>
+    /// <returns>
+    /// The task object representing the asynchronous operation.
+    /// The task's result will be the created product with properties like <see cref="Product.Id" /> set.
+    /// </returns>
+    [HttpPost]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<Product>> CreateProductAsync([Required][FromBody] Product product)
+    {
+        Product createdProduct = await m_catalogServiceClient.CreateProductAsync(product);
+        return Ok(createdProduct);
+    }
+
+    /// <summary>
+    /// Asynchronously updates an existing product.
+    /// </summary>
+    /// <param name="product">The product to update.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> UpdateProductAsync([Required][FromBody] Product product)
+    {
+        try
+        {
+            await m_catalogServiceClient.UpdateProductAsync(product);
+            return Ok();
+        }
+        catch (ApiException apiException) when (apiException.StatusCode == StatusCodes.Status404NotFound)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously deletes an existing product.
+    /// </summary>
+    /// <param name="productId">The unique identifier of the product to delete.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> DeleteProductAsync([Required][FromQuery] Guid productId)
+    {
+        try
+        {
+            await m_catalogServiceClient.DeleteProductAsync(productId);
+            return Ok();
+        }
+        catch (ApiException apiException) when (apiException.StatusCode == StatusCodes.Status404NotFound)
+        {
+            return NotFound();
+        }
+    }
+}
