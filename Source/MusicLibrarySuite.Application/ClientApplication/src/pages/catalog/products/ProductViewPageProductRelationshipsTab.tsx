7832127122cdc9179e7bf3dd69579cc9be98a493
Andrey Talanin <andrey.talanin@outlook.com>
2022-12-24 20:34:23 +0300

    Add the ProductViewPageProductRelationshipsTab React.js component

---
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/products/ProductViewPageProductRelationshipsTab.tsx
@@ -0,0 +1,59 @@
+import { useCallback, useEffect, useState } from "react";
+import { ProductRelationship } from "../../../api/ApplicationClient";
+import useApplicationClient from "../../../hooks/useApplicationClient";
+import { Checkbox, Typography } from "antd";
+import EntityRelationshipTable from "../../../components/tables/EntityRelationshipTable";
+
+export interface ProductViewPageProductRelationshipsTabProps {
+  id: string;
+}
+
+const ProductViewPageProductRelationshipsTab = ({ id }: ProductViewPageProductRelationshipsTabProps) => {
+  const [productRelationships, setProductRelationships] = useState<ProductRelationship[]>([]);
+  const [productRelationshipsLoading, setProductRelationshipsLoading] = useState<boolean>(true);
+  const [includeReverseRelationships, setIncludeReverseRelationships] = useState<boolean>(false);
+
+  const applicationClient = useApplicationClient();
+
+  const fetchProductRelationships = useCallback(() => {
+    setProductRelationshipsLoading(true);
+    applicationClient
+      .getProductRelationships(id, includeReverseRelationships)
+      .then((productRelationships) => {
+        setProductRelationships(productRelationships);
+        setProductRelationshipsLoading(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [id, includeReverseRelationships, applicationClient]);
+
+  useEffect(() => {
+    fetchProductRelationships();
+  }, [fetchProductRelationships]);
+
+  return (
+    <>
+      <Typography.Paragraph>
+        <Checkbox onChange={(e) => setIncludeReverseRelationships(e.target.checked)}>Include reverse product relationships</Checkbox>
+      </Typography.Paragraph>
+      <EntityRelationshipTable
+        entityColumnName="Product"
+        dependentEntityColumnName="Dependent Product"
+        loading={productRelationshipsLoading}
+        entityRelationships={productRelationships.map((productRelationship) => ({
+          name: productRelationship.name,
+          description: productRelationship.description,
+          entityId: productRelationship.productId,
+          entityName: productRelationship.product?.title ?? "",
+          entityHref: `/catalog/products/view?id=${productRelationship.productId}`,
+          dependentEntityId: productRelationship.dependentProductId,
+          dependentEntityName: productRelationship.dependentProduct?.title ?? "",
+          dependentEntityHref: `/catalog/products/view?id=${productRelationship.dependentProductId}`,
+        }))}
+      />
+    </>
+  );
+};
+
+export default ProductViewPageProductRelationshipsTab;
