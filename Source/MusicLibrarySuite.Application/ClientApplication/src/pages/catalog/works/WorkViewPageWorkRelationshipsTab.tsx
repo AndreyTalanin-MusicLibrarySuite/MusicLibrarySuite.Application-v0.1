2b61972b5ecb16d064cd965d2c0c6b0b5d0e24ae
Andrey Talanin <andrey.talanin@outlook.com>
2022-12-27 18:11:24 +0300

    Add the WorkViewPageWorkRelationshipsTab React.js component

---
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/works/WorkViewPageWorkRelationshipsTab.tsx
@@ -0,0 +1,59 @@
+import { Checkbox, Typography } from "antd";
+import { useCallback, useEffect, useState } from "react";
+import { WorkRelationship } from "../../../api/ApplicationClient";
+import EntityRelationshipTable from "../../../components/tables/EntityRelationshipTable";
+import useApplicationClient from "../../../hooks/useApplicationClient";
+
+export interface WorkViewPageWorkRelationshipsTabProps {
+  id: string;
+}
+
+const WorkViewPageWorkRelationshipsTab = ({ id }: WorkViewPageWorkRelationshipsTabProps) => {
+  const [workRelationships, setWorkRelationships] = useState<WorkRelationship[]>([]);
+  const [workRelationshipsLoading, setWorkRelationshipsLoading] = useState<boolean>(true);
+  const [includeReverseRelationships, setIncludeReverseRelationships] = useState<boolean>(false);
+
+  const applicationClient = useApplicationClient();
+
+  const fetchWorkRelationships = useCallback(() => {
+    setWorkRelationshipsLoading(true);
+    applicationClient
+      .getWorkRelationships(id, includeReverseRelationships)
+      .then((workRelationships) => {
+        setWorkRelationships(workRelationships);
+        setWorkRelationshipsLoading(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [id, includeReverseRelationships, applicationClient]);
+
+  useEffect(() => {
+    fetchWorkRelationships();
+  }, [fetchWorkRelationships]);
+
+  return (
+    <>
+      <Typography.Paragraph>
+        <Checkbox onChange={(e) => setIncludeReverseRelationships(e.target.checked)}>Include reverse work relationships</Checkbox>
+      </Typography.Paragraph>
+      <EntityRelationshipTable
+        entityColumnName="Work"
+        dependentEntityColumnName="Dependent Work"
+        loading={workRelationshipsLoading}
+        entityRelationships={workRelationships.map((workRelationship) => ({
+          name: workRelationship.name,
+          description: workRelationship.description,
+          entityId: workRelationship.workId,
+          entityName: workRelationship.work?.title ?? "",
+          entityHref: `/catalog/works/view?id=${workRelationship.workId}`,
+          dependentEntityId: workRelationship.dependentWorkId,
+          dependentEntityName: workRelationship.dependentWork?.title ?? "",
+          dependentEntityHref: `/catalog/works/view?id=${workRelationship.dependentWorkId}`,
+        }))}
+      />
+    </>
+  );
+};
+
+export default WorkViewPageWorkRelationshipsTab;
