8101dd2650cc33a6aeaf3ba22064a6ad70c36713
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-15 20:47:50 +0300

    Edit the ReleaseViewPage React.js page component

--- Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/releases/ReleaseViewPage.tsx
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/releases/ReleaseViewPage.tsx
@@ -1,8 +1,10 @@
-import { Button, Card, Divider, Space, Tag, Tooltip, Typography } from "antd";
-import { EditOutlined, QuestionCircleOutlined, RollbackOutlined } from "@ant-design/icons";
+import { Button, Card, Collapse, Divider, Space, Tag, Tooltip, Typography } from "antd";
+import { EditOutlined, MonitorOutlined, QuestionCircleOutlined, RollbackOutlined } from "@ant-design/icons";
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { Release } from "../../../api/ApplicationClient";
+import { Release, ReleaseMedia } from "../../../api/ApplicationClient";
+import CreateReleaseMediaModal from "../../../components/modals/CreateReleaseMediaModal";
+import { formatReleaseMediaNumber, getReleaseMediaKey } from "../../../helpers/ReleaseMediaHelpers";
 import useApplicationClient from "../../../hooks/useApplicationClient";
 import useQueryStringId from "../../../hooks/useQueryStringId";
 import styles from "./ReleaseViewPage.module.css";
@@ -14,6 +16,8 @@ const ReleaseViewPage = () => {
   const navigate = useNavigate();
 
   const [release, setRelease] = useState<Release>();
+  const [viewReleaseMediaModalOpen, setViewReleaseMediaModalOpen] = useState<boolean>(false);
+  const [releaseMediaToView, setReleaseMediaToView] = useState<ReleaseMedia>();
 
   const [id] = useQueryStringId(true);
   const applicationClient = useApplicationClient();
@@ -43,69 +47,141 @@ const ReleaseViewPage = () => {
     navigate("/catalog/releases/list");
   };
 
+  const onViewReleaseMediaButtonClick = (releaseMedia: ReleaseMedia) => {
+    setReleaseMediaToView(releaseMedia);
+    setViewReleaseMediaModalOpen(true);
+  };
+
   return (
     <>
       <Space className={styles.pageHeader} direction="horizontal" align="baseline">
         <Title level={4}>View Release</Title>
         <Button type="primary" onClick={onEditButtonClick}>
-          <EditOutlined />
-          Edit
+          <EditOutlined /> Edit
         </Button>
         <Button onClick={onCancelButtonClick}>
-          <RollbackOutlined />
-          Back to Release List
+          <RollbackOutlined /> Back to Release List
         </Button>
       </Space>
-      {release && (
-        <Card
-          title={
-            <Space>
-              {release.title}
-              {release.disambiguationText && (
-                <Tooltip title={release.disambiguationText}>
-                  <QuestionCircleOutlined />
-                </Tooltip>
+      <Space direction="vertical" style={{ display: "flex" }}>
+        {release && (
+          <Card
+            title={
+              <Space>
+                {release.title}
+                {release.disambiguationText && (
+                  <Tooltip title={release.disambiguationText}>
+                    <QuestionCircleOutlined />
+                  </Tooltip>
+                )}
+                <Tag color={release.enabled ? "green" : "red"}>{release.enabled ? "Enabled" : "Disabled"}</Tag>
+              </Space>
+            }
+          >
+            {release.description?.length && <Paragraph>{release.description}</Paragraph>}
+            {release.barcode?.length && (
+              <Paragraph>
+                Barcode: <Text keyboard>{release.barcode}</Text>
+              </Paragraph>
+            )}
+            {release.catalogNumber?.length && (
+              <Paragraph>
+                Catalog Number: <Text keyboard>{release.catalogNumber}</Text>
+              </Paragraph>
+            )}
+            {release.mediaFormat?.length && (
+              <Paragraph>
+                Media Format: <Text>{release.mediaFormat}</Text>
+              </Paragraph>
+            )}
+            {release.publishFormat?.length && (
+              <Paragraph>
+                Media Format: <Text>{release.publishFormat}</Text>
+              </Paragraph>
+            )}
+            <Paragraph>
+              Released On: <Text keyboard>{release.releasedOnYearOnly ? release.releasedOn.getUTCFullYear() : release.releasedOn.toLocaleDateString()}</Text>
+            </Paragraph>
+            <Divider />
+            {release.createdOn && (
+              <Paragraph>
+                Created On: <Text keyboard>{release.createdOn.toLocaleString()}</Text>
+              </Paragraph>
+            )}
+            {release.updatedOn && (
+              <Paragraph>
+                Updated On: <Text keyboard>{release.updatedOn.toLocaleString()}</Text>
+              </Paragraph>
+            )}
+          </Card>
+        )}
+        {release &&
+          release.releaseMediaCollection.map((releaseMedia) => (
+            <Card
+              key={getReleaseMediaKey(releaseMedia)}
+              size="small"
+              title={
+                <Space wrap className={styles.cardHeader} direction="horizontal" align="baseline">
+                  <Space wrap direction="horizontal" align="baseline">
+                    {`${formatReleaseMediaNumber(releaseMedia.mediaNumber, release.releaseMediaCollection.length)} - ${releaseMedia.title}`}
+                    {releaseMedia.disambiguationText && (
+                      <Tooltip title={releaseMedia.disambiguationText}>
+                        <QuestionCircleOutlined />
+                      </Tooltip>
+                    )}
+                  </Space>
+                  <Button onClick={() => onViewReleaseMediaButtonClick(releaseMedia)}>
+                    <MonitorOutlined /> View
+                  </Button>
+                </Space>
+              }
+            >
+              {(releaseMedia.description ||
+                releaseMedia.catalogNumber ||
+                releaseMedia.mediaFormat ||
+                releaseMedia.tableOfContentsChecksum ||
+                releaseMedia.tableOfContentsChecksumLong) && (
+                <Collapse>
+                  <Collapse.Panel key="release-media-details" header="Release Media Details">
+                    {releaseMedia.description && <Paragraph>{releaseMedia.description}</Paragraph>}
+                    {releaseMedia.description &&
+                      (releaseMedia.catalogNumber ||
+                        releaseMedia.mediaFormat ||
+                        releaseMedia.tableOfContentsChecksum ||
+                        releaseMedia.tableOfContentsChecksumLong) && <Divider />}
+                    {releaseMedia.catalogNumber && (
+                      <Paragraph>
+                        Catalog Number: <Text keyboard>{releaseMedia.catalogNumber}</Text>
+                      </Paragraph>
+                    )}
+                    {releaseMedia.mediaFormat && (
+                      <Paragraph>
+                        Media Format: <Text>{releaseMedia.mediaFormat}</Text>
+                      </Paragraph>
+                    )}
+                    {releaseMedia.tableOfContentsChecksum && (
+                      <Paragraph>
+                        CDDB Checksum: <Text keyboard>{releaseMedia.tableOfContentsChecksum}</Text>
+                      </Paragraph>
+                    )}
+                    {releaseMedia.tableOfContentsChecksumLong && (
+                      <Paragraph>
+                        MusicBrainz Checksum: <Text keyboard>{releaseMedia.tableOfContentsChecksumLong}</Text>
+                      </Paragraph>
+                    )}
+                  </Collapse.Panel>
+                </Collapse>
               )}
-              <Tag color={release.enabled ? "green" : "red"}>{release.enabled ? "Enabled" : "Disabled"}</Tag>
-            </Space>
-          }
-        >
-          {release.description?.length && <Paragraph>{release.description}</Paragraph>}
-          {release.barcode?.length && (
-            <Paragraph>
-              Barcode: <Text keyboard>{release.barcode}</Text>
-            </Paragraph>
-          )}
-          {release.catalogNumber?.length && (
-            <Paragraph>
-              Catalog Number: <Text keyboard>{release.catalogNumber}</Text>
-            </Paragraph>
-          )}
-          {release.mediaFormat?.length && (
-            <Paragraph>
-              Media Format: <Text>{release.mediaFormat}</Text>
-            </Paragraph>
-          )}
-          {release.publishFormat?.length && (
-            <Paragraph>
-              Media Format: <Text>{release.publishFormat}</Text>
-            </Paragraph>
-          )}
-          <Paragraph>
-            Released On: <Text keyboard>{release.releasedOnYearOnly ? release.releasedOn.getUTCFullYear() : release.releasedOn.toLocaleDateString()}</Text>
-          </Paragraph>
-          <Divider />
-          {release.createdOn && (
-            <Paragraph>
-              Created On: <Text keyboard>{release.createdOn.toLocaleString()}</Text>
-            </Paragraph>
-          )}
-          {release.updatedOn && (
-            <Paragraph>
-              Updated On: <Text keyboard>{release.updatedOn.toLocaleString()}</Text>
-            </Paragraph>
-          )}
-        </Card>
+            </Card>
+          ))}
+      </Space>
+      {release && (
+        <CreateReleaseMediaModal
+          open={viewReleaseMediaModalOpen}
+          releaseMedia={releaseMediaToView}
+          onOk={() => setViewReleaseMediaModalOpen(false)}
+          onCancel={() => setViewReleaseMediaModalOpen(false)}
+        />
       )}
     </>
   );
