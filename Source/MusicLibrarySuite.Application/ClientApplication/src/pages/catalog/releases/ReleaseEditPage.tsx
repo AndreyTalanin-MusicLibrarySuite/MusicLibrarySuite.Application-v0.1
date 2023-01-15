37890165abf6f939fa2c2a34eae40eb86568f516
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-15 20:47:39 +0300

    Edit the ReleaseEditPage React.js page component

--- Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/releases/ReleaseEditPage.tsx
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/releases/ReleaseEditPage.tsx
@@ -1,18 +1,30 @@
-import { Button, Checkbox, Col, DatePicker, Form, Input, Row, Space, Typography } from "antd";
+import { Button, Card, Checkbox, Col, Collapse, DatePicker, Divider, Form, Input, Row, Space, Tooltip, Typography } from "antd";
 import { Store } from "antd/lib/form/interface";
-import { DeleteOutlined, RollbackOutlined, SaveOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  FieldNumberOutlined,
+  FolderAddOutlined,
+  QuestionCircleOutlined,
+  RollbackOutlined,
+  SaveOutlined,
+} from "@ant-design/icons";
 import dayjs, { Dayjs } from "dayjs";
 import weekday from "dayjs/plugin/weekday";
 import localeData from "dayjs/plugin/localeData";
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { IRelease, Release } from "../../../api/ApplicationClient";
+import { IRelease, Release, ReleaseMedia } from "../../../api/ApplicationClient";
 import ConfirmDeleteModal from "../../../components/modals/ConfirmDeleteModal";
+import CreateReleaseMediaModal from "../../../components/modals/CreateReleaseMediaModal";
 import { EmptyGuidString } from "../../../helpers/ApplicationConstants";
+import { formatReleaseMediaNumber, getReleaseMediaKey } from "../../../helpers/ReleaseMediaHelpers";
 import useApplicationClient from "../../../hooks/useApplicationClient";
 import useQueryStringId from "../../../hooks/useQueryStringId";
 import styles from "./ReleaseEditPage.module.css";
 import "antd/dist/antd.min.css";
+
+const { Paragraph, Text, Title } = Typography;
 
 dayjs.extend(weekday);
 dayjs.extend(localeData);
@@ -33,6 +45,8 @@ const ReleaseEditPage = ({ mode }: ReleaseEditPageProps) => {
   const [releaseFormValues, setReleaseFormValues] = useState<Store>({});
   const [loading, setLoading] = useState<boolean>(false);
   const [confirmDeleteModalOpen, setConfirmDeleteModalOpen] = useState<boolean>(false);
+  const [createReleaseMediaModalOpen, setCreateReleaseMediaModalOpen] = useState<boolean>(false);
+  const [releaseMediaToEdit, setReleaseMediaToEdit] = useState<ReleaseMedia>();
 
   const [id] = useQueryStringId(mode === ReleaseEditPageMode.Edit);
   const applicationClient = useApplicationClient();
@@ -71,34 +85,60 @@ const ReleaseEditPage = ({ mode }: ReleaseEditPageProps) => {
     }
   }, [release]);
 
-  const onConfirmDeleteModalOk = useCallback(
-    (setModalLoading: (value: boolean) => void) => {
-      if (release) {
-        setModalLoading(true);
-        applicationClient
-          .deleteRelease(release.id)
-          .then(() => {
-            setModalLoading(false);
-            setConfirmDeleteModalOpen(false);
-            navigate("/catalog/releases/list");
-          })
-          .catch((error) => {
-            setModalLoading(false);
-            setConfirmDeleteModalOpen(false);
-            alert(error);
-          });
-      }
-    },
-    [navigate, release, applicationClient]
-  );
-
-  const onConfirmDeleteModalCancel = () => {
-    setConfirmDeleteModalOpen(false);
-  };
-
   const onCancelButtonClick = () => {
     navigate("/catalog/releases/list");
   };
+
+  const onCreateReleaseMediaButtonClick = useCallback(() => {
+    if (release) {
+      setReleaseMediaToEdit(undefined);
+      setCreateReleaseMediaModalOpen(true);
+    }
+  }, [release]);
+
+  const onRenumberReleaseContentButtonClick = useCallback(() => {
+    if (release) {
+      const orderedReleaseMediaCollection = [...release.releaseMediaCollection].sort(
+        (item, otherItem) => (item.mediaNumber ?? 0) - (otherItem.mediaNumber ?? 0)
+      );
+      setRelease(
+        new Release({
+          ...release,
+          releaseMediaCollection: orderedReleaseMediaCollection.map(
+            (releaseMedia, index) =>
+              new ReleaseMedia({
+                ...releaseMedia,
+                mediaNumber: index + 1,
+              })
+          ),
+        })
+      );
+    }
+  }, [release]);
+
+  const onEditReleaseMediaButtonClick = useCallback(
+    (releaseMedia: ReleaseMedia) => {
+      if (release) {
+        setReleaseMediaToEdit(releaseMedia);
+        setCreateReleaseMediaModalOpen(true);
+      }
+    },
+    [release]
+  );
+
+  const onDeleteReleaseMediaButtonClick = useCallback(
+    (releaseMedia: ReleaseMedia) => {
+      if (release) {
+        setRelease(
+          new Release({
+            ...release,
+            releaseMediaCollection: release.releaseMediaCollection.filter((releaseMediaToCompare) => releaseMediaToCompare !== releaseMedia),
+          })
+        );
+      }
+    },
+    [release]
+  );
 
   const onFinish = useCallback(
     (releaseFormValues: Store) => {
@@ -169,23 +209,81 @@ const ReleaseEditPage = ({ mode }: ReleaseEditPageProps) => {
     alert("Form validation failed. Please ensure that you have filled all the required fields.");
   };
 
+  const onConfirmDeleteModalOk = useCallback(
+    (setModalLoading: (value: boolean) => void) => {
+      if (release) {
+        setModalLoading(true);
+        applicationClient
+          .deleteRelease(release.id)
+          .then(() => {
+            setModalLoading(false);
+            setConfirmDeleteModalOpen(false);
+            navigate("/catalog/releases/list");
+          })
+          .catch((error) => {
+            setModalLoading(false);
+            setConfirmDeleteModalOpen(false);
+            alert(error);
+          });
+      }
+    },
+    [navigate, release, applicationClient]
+  );
+
+  const onConfirmDeleteModalCancel = () => {
+    setConfirmDeleteModalOpen(false);
+  };
+
+  const onCreateReleaseMediaModalOk = useCallback(
+    (releaseMedia: ReleaseMedia, resetFormFields: () => void) => {
+      if (release) {
+        const existingReleaseMedia = release.releaseMediaCollection.find(
+          (releaseMediaToCompare) => releaseMediaToCompare.mediaNumber === releaseMedia.mediaNumber
+        );
+        if (existingReleaseMedia && !releaseMediaToEdit) {
+          alert(`Unable to create a release media with a non-unique number: ${releaseMedia.mediaNumber}.`);
+          return;
+        }
+
+        const shouldAddReleaseMedia = !releaseMediaToEdit;
+
+        if (shouldAddReleaseMedia) {
+          setRelease(new Release({ ...release, releaseMediaCollection: [...release.releaseMediaCollection, releaseMedia] }));
+        } else {
+          setRelease(
+            new Release({
+              ...release,
+              releaseMediaCollection: release.releaseMediaCollection.map((releaseMediaToCompare) =>
+                releaseMediaToCompare !== releaseMediaToEdit ? releaseMediaToCompare : releaseMedia
+              ),
+            })
+          );
+        }
+        setCreateReleaseMediaModalOpen(false);
+        resetFormFields();
+      }
+    },
+    [release, releaseMediaToEdit]
+  );
+
+  const onCreateReleaseMediaModalCancel = () => {
+    setCreateReleaseMediaModalOpen(false);
+  };
+
   return (
     <>
       <Space className={styles.pageHeader} direction="horizontal" align="baseline">
-        <Typography.Title level={4}>{mode === ReleaseEditPageMode.Create ? "Create" : "Edit"} Release</Typography.Title>
+        <Title level={4}>{mode === ReleaseEditPageMode.Create ? "Create" : "Edit"} Release</Title>
         <Button type="primary" loading={loading} onClick={onSaveButtonClick}>
-          <SaveOutlined />
-          Save
+          <SaveOutlined /> Save
         </Button>
         {mode === ReleaseEditPageMode.Edit && (
           <Button danger type="primary" onClick={onDeleteButtonClick}>
-            <DeleteOutlined />
-            Delete
+            <DeleteOutlined /> Delete
           </Button>
         )}
         <Button onClick={onCancelButtonClick}>
-          <RollbackOutlined />
-          Cancel
+          <RollbackOutlined /> Cancel
         </Button>
       </Space>
       <Row>
@@ -268,6 +366,87 @@ const ReleaseEditPage = ({ mode }: ReleaseEditPageProps) => {
           </Form>
         </Col>
       </Row>
+      {release && mode === ReleaseEditPageMode.Edit && (
+        <Space direction="vertical" style={{ display: "flex" }}>
+          <Card
+            size="small"
+            title={
+              <Space wrap className={styles.cardHeader} direction="horizontal" align="baseline">
+                Release Content
+                <Button onClick={onCreateReleaseMediaButtonClick}>
+                  <FolderAddOutlined /> Create Media
+                </Button>
+                <Button onClick={onRenumberReleaseContentButtonClick}>
+                  <FieldNumberOutlined /> Reorder & Renumber Content
+                </Button>
+              </Space>
+            }
+          >
+            <Paragraph>The current release contains {release.releaseMediaCollection.length ?? 0} release media.</Paragraph>
+          </Card>
+          {release.releaseMediaCollection.map((releaseMedia) => (
+            <Card
+              key={getReleaseMediaKey(releaseMedia)}
+              size="small"
+              title={
+                <Space wrap className={styles.cardHeader} direction="horizontal" align="baseline">
+                  <Space wrap direction="horizontal" align="baseline">
+                    {`${formatReleaseMediaNumber(releaseMedia.mediaNumber, releaseMedia.totalMediaCount)} - ${releaseMedia.title}`}
+                    {releaseMedia.disambiguationText && (
+                      <Tooltip title={releaseMedia.disambiguationText}>
+                        <QuestionCircleOutlined />
+                      </Tooltip>
+                    )}
+                  </Space>
+                  <Button onClick={() => onEditReleaseMediaButtonClick(releaseMedia)}>
+                    <EditOutlined /> Edit
+                  </Button>
+                  <Button danger onClick={() => onDeleteReleaseMediaButtonClick(releaseMedia)}>
+                    <DeleteOutlined /> Delete
+                  </Button>
+                </Space>
+              }
+            >
+              {(releaseMedia.description ||
+                releaseMedia.catalogNumber ||
+                releaseMedia.mediaFormat ||
+                releaseMedia.tableOfContentsChecksum ||
+                releaseMedia.tableOfContentsChecksumLong) && (
+                <Collapse>
+                  <Collapse.Panel key="release-media-details" header="Release Media Details">
+                    {releaseMedia.description && <Paragraph>{releaseMedia.description}</Paragraph>}
+                    {releaseMedia.description &&
+                      (releaseMedia.catalogNumber ||
+                        releaseMedia.mediaFormat ||
+                        releaseMedia.tableOfContentsChecksum ||
+                        releaseMedia.tableOfContentsChecksumLong) && <Divider />}
+                    {releaseMedia.catalogNumber && (
+                      <Paragraph>
+                        Catalog Number: <Text keyboard>{releaseMedia.catalogNumber}</Text>
+                      </Paragraph>
+                    )}
+                    {releaseMedia.mediaFormat && (
+                      <Paragraph>
+                        Media Format: <Text>{releaseMedia.mediaFormat}</Text>
+                      </Paragraph>
+                    )}
+                    {releaseMedia.tableOfContentsChecksum && (
+                      <Paragraph>
+                        CDDB Checksum: <Text keyboard>{releaseMedia.tableOfContentsChecksum}</Text>
+                      </Paragraph>
+                    )}
+                    {releaseMedia.tableOfContentsChecksumLong && (
+                      <Paragraph>
+                        MusicBrainz Checksum: <Text keyboard>{releaseMedia.tableOfContentsChecksumLong}</Text>
+                      </Paragraph>
+                    )}
+                  </Collapse.Panel>
+                </Collapse>
+              )}
+            </Card>
+          ))}
+        </Space>
+      )}
       {release && (
         <ConfirmDeleteModal
           open={confirmDeleteModalOpen}
@@ -275,6 +454,15 @@ const ReleaseEditPage = ({ mode }: ReleaseEditPageProps) => {
           message={`Confirm that you want to delete the "${release.title}" release. This operation can not be undone.`}
           onOk={onConfirmDeleteModalOk}
           onCancel={onConfirmDeleteModalCancel}
+        />
+      )}
+      {release && (
+        <CreateReleaseMediaModal
+          edit
+          open={createReleaseMediaModalOpen}
+          releaseMedia={releaseMediaToEdit}
+          onOk={onCreateReleaseMediaModalOk}
+          onCancel={onCreateReleaseMediaModalCancel}
         />
       )}
     </>
