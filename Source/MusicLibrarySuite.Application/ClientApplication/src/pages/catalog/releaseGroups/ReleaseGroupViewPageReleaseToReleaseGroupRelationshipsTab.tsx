04dcb8d3c6fe9f1b277e05a1afabcfac4be9768e
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-29 00:23:48 +0300

    Add the ReleaseGroupViewPageReleaseToReleaseGroupRelationshipsTab React.js component

---
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/pages/catalog/releaseGroups/ReleaseGroupViewPageReleaseToReleaseGroupRelationshipsTab.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { ReleaseToReleaseGroupRelationship } from "../../../api/ApplicationClient";
+import EntityRelationshipTable from "../../../components/tables/EntityRelationshipTable";
+import useApplicationClient from "../../../hooks/useApplicationClient";
+
+export interface ReleaseGroupViewPageReleaseToReleaseGroupRelationshipsTabProps {
+  id: string;
+}
+
+const ReleaseGroupViewPageReleaseToReleaseGroupRelationshipsTab = ({ id }: ReleaseGroupViewPageReleaseToReleaseGroupRelationshipsTabProps) => {
+  const [releaseToReleaseGroupRelationships, setReleaseToReleaseGroupRelationships] = useState<ReleaseToReleaseGroupRelationship[]>([]);
+  const [releaseToReleaseGroupRelationshipsLoading, setReleaseToReleaseGroupRelationshipsLoading] = useState<boolean>(true);
+
+  const applicationClient = useApplicationClient();
+
+  useEffect(() => {
+    setReleaseToReleaseGroupRelationshipsLoading(true);
+    applicationClient
+      .getReleaseToReleaseGroupRelationshipsByReleaseGroup(id)
+      .then((releaseToReleaseGroupRelationships) => {
+        setReleaseToReleaseGroupRelationships(releaseToReleaseGroupRelationships);
+        setReleaseToReleaseGroupRelationshipsLoading(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [id, applicationClient]);
+
+  return (
+    <EntityRelationshipTable
+      entityColumnName="Release"
+      dependentEntityColumnName="Release Group"
+      loading={releaseToReleaseGroupRelationshipsLoading}
+      entityRelationships={releaseToReleaseGroupRelationships.map((releaseToReleaseGroupRelationship) => ({
+        name: releaseToReleaseGroupRelationship.name,
+        description: releaseToReleaseGroupRelationship.description,
+        entityId: releaseToReleaseGroupRelationship.releaseId,
+        entityName: releaseToReleaseGroupRelationship.release?.title ?? "",
+        entityHref: `/catalog/releases/view?id=${releaseToReleaseGroupRelationship.releaseId}`,
+        dependentEntityId: releaseToReleaseGroupRelationship.releaseGroupId,
+        dependentEntityName: releaseToReleaseGroupRelationship.releaseGroup?.title ?? "",
+        dependentEntityHref: `/catalog/releaseGroups/view?id=${releaseToReleaseGroupRelationship.releaseGroupId}`,
+      }))}
+    />
+  );
+};
+
+export default ReleaseGroupViewPageReleaseToReleaseGroupRelationshipsTab;
