b4e33e38f485d7abf654177b4e7d3bb5f104adc1
Andrey Talanin <andrey.talanin@outlook.com>
2023-02-02 01:16:56 +0300

    Edit the ReleaseTrackHelpers.ts file

--- Source/MusicLibrarySuite.Application/ClientApplication/src/helpers/ReleaseTrackHelpers.ts
+++ Source/MusicLibrarySuite.Application/ClientApplication/src/helpers/ReleaseTrackHelpers.ts
@@ -4,6 +4,10 @@ export const getReleaseTrackKey = (releaseTrack: ReleaseTrack) => {
   return `${releaseTrack.trackNumber}-${releaseTrack.mediaNumber}-${releaseTrack.releaseId}`;
 };
 
+export const getReleaseTrackKeyByComponents = (trackNumber: number, mediaNumber: number, releaseId: string) => {
+  return `${trackNumber}-${mediaNumber}-${releaseId}`;
+};
+
 export const formatReleaseTrackNumber = (number: number, totalCount: number | undefined) => {
   return number.toLocaleString(undefined, { minimumIntegerDigits: totalCount !== undefined && totalCount > 99 ? 3 : 2 });
 };
